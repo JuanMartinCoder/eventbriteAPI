282260963bcc95c0328482002c9ec65d5511b187
Skyz <48739250+SkyzInformatica@users.noreply.github.com>
2019-08-21 09:23:30 -0300

    primera version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import Header from './components/Header'
+import FormularioAtr from './components/FormularioAtr'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CategoriaPovider from './context/CategoriaContext'
+
+class App extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <CategoriaPovider>
+        <Header/>
+        
+        <div className="uk-container">
+          <FormularioAtr/>
+        </div>
+      </CategoriaPovider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FormularioAtr.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class FormularioAtr extends Component {
+    state ={
+        nombre: '',
+        categoria : ''
+    }
+    render() {
+        return (
+            <form>
+                <fieldset className="uk-fieldset uk-margin">
+                    <legend className="uk-legend uk-text-center">
+                        Busca tu evento por nombre o por categoria
+                    </legend>
+                </fieldset>
+
+                <div className="uk-column-1-3@m uk-margin">
+                    <div className="uk-margin" uk-margin="true">
+                        <input name="nombre" className="uk-input" type="text" placeholder="Nombre de evento o ciudad"/>
+                    </div>
+                    <div className="uk-margin" uk-margin="true">
+                        <select name="categoria" className="uk-select"></select>
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Buscar Eventos" className="uk-button uk-button-danger"/>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default FormularioAtr;
